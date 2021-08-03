e384c1772aa4c2c6d26a4414782f5f1b3e2242db
x0rk <kavinassundaramurthy@protonmail.com>
2021-08-03 15:33:22 +0530

    more efficient

--- Root_to_1.py
+++ Root_to_1.py
@@ -1,34 +1,23 @@
 r = 2
 
-
-def pw(x):
-    p = 1 / x
-    return p
-
-
-def rt(y, z):
+def root(y, z):
     c = 0
     t = y
-    for i in range(2, 9999):
-        a = pow(y, z)
-        y = a
+    while True:
+        y = a = pow(y, z)
         c += 1
         if a == 1:
             break
-    z += 1
     if r == 2:
-        print('\nAfter taking \'', c, '\'', 'Square roots', t, 'becomes 1 \n')
+        print('\nAfter taking \'', c, '\'', 'Square roots', t, 'becomes 1\n')
+    elif r == 3:
+        print('\nAfter taking \'', c, '\'', 'Cube roots', t, 'becomes 1\n')
     else:
-        if r == 3:
-            print('\nAfter taking \'', c, '\'', 'Cube roots', t, 'becomes 1 \n')
-        else:
-            print('\nAfter taking \'', c, '\'', r, 'th roots', t, 'becomes 1 \n')
-    return
-
+        print('\nAfter taking \'', c, '\'', r, 'th roots', t, 'becomes 1\n')
 
 num = int(input('Enter a number: '))
 n = int(input('Enter the nth root: '))
-n += 1
-for j in range(2, n):
-    rt(num, pw(j))
+
+for j in range(2, n+1):
+    root(num, 1/j)
     r += 1
